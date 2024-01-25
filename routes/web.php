fa4d69e03316ca60e2bd02a901adc507dd676948
Gayakhan Eyvazov <gayakhan.eyvazov@ram.lan>
2024-01-25 17:10:36 +0400

    Template integration for Laravel Blade Structur. About page completed

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index_page');
 });
+
+
+Route::get('/aboutpage', function () {
+    return view('about_page');
+});
 Route::get('/index', function () {
     return view('index');
 });
