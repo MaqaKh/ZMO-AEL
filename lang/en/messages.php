9efaa2314660187d502680c52e00f383073a7772
mahammad <mkhalilov@karty.qa>
2024-02-06 21:14:36 +0300

    add messages

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -6,4 +6,8 @@ return [
     'home' => 'Home',
     'about' => 'About',
     'products' => 'Products',
+    'transmission_oil' => 'Transmission oil',
+    'engine_oil' => 'Engine oil',
+    'promotional_accesories' => 'Promotional accessories',
+    'contacts' => 'Contacts',
 ];
